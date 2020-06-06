14b2e2a3fddd01e8836b7265993a55e8d23fb92c
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-05 23:10:38 -0300

    Projeto finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int N = 0, X = 0, i;
+
+		N = sc.nextInt();
+
+		for (i = 0; i < N; i++) {
+			X = sc.nextInt();
+
+			if (X > 0 && X % 2 == 1) {
+				System.out.println("ODD POSITIVE");
+			} else if (X > 0 && X % 2 == 0) {
+				System.out.println("EVEN POSITIVE");
+			} else if (X < 0 && X % 2 == -1) {
+				System.out.println("ODD NEGATIVE");
+			} else if (X < 0 && X % 2 == 0) {
+				System.out.println("EVEN NEGATIVE");
+			} else {
+				System.out.println("NULL");
+			}
+
+		}
+
+		sc.close();
 
 	}
 
